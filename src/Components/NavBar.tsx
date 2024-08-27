eea65faf10e34ff00ed6f55d14dbddbe3b6f0133
Boyman Pauler <you@example.com>
2024-08-27 08:43:00 -0400

    resolve import issue

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
-import MiniSVG from '../assets/miniaisvg.svg'
+import MiniSVG from '../assets/MiniAiSVG.svg'
 import Socials from './Socials';
 
 function NavBar() {

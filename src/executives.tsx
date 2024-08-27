5832144ae0691f96b12b73af6adc14a95079c9b0
Boyman Pauler <you@example.com>
2024-08-27 08:23:46 -0400

    dist

--- src/executives.tsx
+++ src/executives.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ExecutivesApp from './ExecutivesApp.tsx'
+import ExecutivesApp from './ExecutivesApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
